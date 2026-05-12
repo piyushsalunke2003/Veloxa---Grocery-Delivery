79faa3246cce6210443817e931cf42cec499e695
piyushsalunke2003 <piyushsalunke2003@gmail.com>
2026-05-12 18:09:30 +0530

    working on login page

--- client/src/pages/AppLayout.tsx
+++ client/src/pages/AppLayout.tsx
@@ -1,10 +1,16 @@
-
+import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <p>banner</p>
+      <p>navbar</p>
+      <main className='min-h-screen'>
+        <Outlet />
+      </main>
+      <p>footer</p>
+      <p>cartsidebar</p>
+    </>
   )
 }
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,45 @@
-
+import { useState } from 'react'
+import { heroSectionData } from '../assets/assets'
+import { Link } from 'react-router-dom'
+import { BikeIcon } from 'lucide-react'
 
 const Login = () => {
+    const [isLoginState, setIsLoginState] = useState(true)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (e: React.SubmitEvent) => {
+        e.preventDefault()
+        setLoading(true);
+        setTimeout(() => window.location.href = "/", 1000)
+    }
+
   return (
-    <div>
-      
+    <div className='min-h-screen flex'>
+      {/* { Left Side} */}
+      <div className='hidden lg:flex lg:w-1/2 bg-app-green relative items-center justify-center'>
+        <img src={heroSectionData.hero_image} alt="" className='absolute inset-0 object-cover h-full bg-center opacity-10'/>
+        <div className='relative text-center px-12'>
+            <h2 className='text-4xl font-semibold text-white mb-4'>Welcome back to Veloxa</h2>
+            <p className='text-white/60 font-serif text-xl max-w-sm mx-auto'>Fresh groceries and organic produce, delivered to your doorstep.</p>
+        </div>
+      </div>
+        {/* { Right Side} */}
+        <div className='flex-1 flex-center px-4 py-12 bg-app-cream'>
+            <div className='w-full max-w-md'>
+                {/* {form header message} */}
+                <div className='text-center mb-8'>
+                    <Link to ="/" className='inline-flex items-center gap-2 mb-6'>
+                        <BikeIcon className="size-8 text-app-green"/>
+                        <span className='text-2xl font-semibold text-app-green'>Veloxa</span>
+                    </Link>
+                    <h1></h1>
+                    <p></p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
